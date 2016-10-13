4df66d58d02e86237d35b1abb5a38fe66ea1c4eb
Baron Dimaranan <barondimaranan@gmail.com>
2016-10-14 05:29:31 +0800

    Comm2

--- app/src/main/java/com/springs/springs/Home.java
+++ app/src/main/java/com/springs/springs/Home.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 
+import android.support.v7.widget.AppCompatDrawableManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
